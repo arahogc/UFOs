a1c266b608e9c3042f0cf6269aa98cc9254c08b0
arahogc <74072742+arahogc@users.noreply.github.com>
2021-02-04 21:05:25 -0800

    updated app.js code

--- static/js/app.js
+++ static/js/app.js
@@ -24,3 +24,24 @@ function buildTable(data){
     });
 }
 
+// create a function to "listen" to clicks us d3 library
+function handleClick() {
+    let date= d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    // if-statement, === looks for exact matches only
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    //Rebuild the table using the filtered data
+    //@Note: if no date was entered, then filteredData will
+    //just be the original tableData. 
+    buildTable(filteredData);
+};
+
+//Attach an event to listen for the form button
+d3.selectAll("#filtered-btn").on("click", handleClick);
+
+//build the table when the page loads
+buildTable(tableData);
+
+
